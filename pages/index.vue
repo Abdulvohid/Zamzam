47e0e00f38cc7924ac150983bdc2ab7a3315d1f5
Abdulvohid <abdulvohid.karimjonov.97@gmail.com>
2022-10-25 10:52:23 +0500

    starter

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+
+
+
+<LangButton/>
+
+<h2>{{ $t('hello') }}</h2>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  components: 'LangButton'
+}
+</script>
